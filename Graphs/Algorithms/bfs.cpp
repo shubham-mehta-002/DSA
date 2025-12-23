cffb3cced202ce2a8713dee87d765537816ce6f8
shubham-mehta-002 <mehta.shubham002@gmail.com>
2025-12-23 12:32:26 +0530

    bfs+dfs algo

---
+++ Graphs/Algorithms/bfs.cpp
@@ -0,0 +1,41 @@
+class Solution {
+    vector<int>res;
+  public:
+    void traverse(vector<vector<int>>&adj, vector<int>&vis, int node){
+        queue<int>q;
+        q.push(node);
+        
+        while(!q.empty()){
+            int size = q.size();
+            while(size--){
+                int top = q.front();
+                for(int neigh : adj[top]){
+                    if(vis[neigh] == 0){
+                        res.push_back(neigh);
+                        vis[neigh] = 1;
+                        q.push(neigh);
+                    }
+                }
+                q.pop();
+            }
+        }
+    }
+    
+    vector<int> bfs(vector<vector<int>> &adj) {
+        vector<int>vis(adj.size(),0);
+        for(int i = 0; i <adj.size() ; i++){
+            if(vis[i] == 0){
+                vis[i] = 1; // mark as visited
+                res.push_back(i);
+                traverse(adj,vis,i);
+            }
+        }
+        return res;
+    }
+};
+
+
+
+
+
+// https://www.geeksforgeeks.org/problems/bfs-traversal-of-graph/1
---
+++ Graphs/Algorithms/dfs.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    vector<int>res;
+public:
+    void traverse(vector<vector<int>>&adj, vector<int>&vis, int node){
+        vis[node] = 1;
+        res.push_back(node);
+        for(int el : adj[node]){
+            if(!vis[el])
+            traverse(adj,vis,el);
+        }
+    }
+    
+    vector<int> dfs(vector<vector<int>>& adj) {
+       int n = adj.size();
+       vector<int>vis(n,0);
+       
+       for(int i=0;i<n;i++){
+           if(!vis[i]){
+               traverse(adj,vis,i);
+           }
+       }
+       
+       return res;
+    }
+};
+
+
+// https://www.geeksforgeeks.org/problems/depth-first-traversal-for-a-graph/1
