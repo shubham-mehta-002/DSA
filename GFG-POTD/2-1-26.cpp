3dbf5621c58593baae5da8edead3f227648ff65c
shubham-mehta-002 <mehta.shubham002@gmail.com>
2026-01-02 23:37:55 +0530

    2-1-26

---
+++ GFG-POTD/2-1-26.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        int zero = -1;
+        int one = 0;
+        int two = arr.size();
+        
+        while(one < two){
+            if(arr[one] == 0){
+                swap(arr[zero+1] , arr[one]);
+                one++;
+                zero++;
+            }else if(arr[one] == 1){
+                one++;
+            }else{
+                swap(arr[two-1] ,arr[one]);
+                two--;
+            }
+        }
+        
+        return;
+        
+    }
+};
