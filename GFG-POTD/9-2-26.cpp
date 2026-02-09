23fcb03cd8460386d48c9121f0d6c1cfd7813ec6
shubham-mehta-002 <mehta.shubham002@gmail.com>
2026-02-09 09:28:59 +0530

    9-2-26

---
+++ GFG-POTD/9-2-26.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int findKRotation(vector<int> &arr)
+    {
+        int n = arr.size();
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                return i + 1;
+            }
+        }
+
+        return 0;
+    }
+};
