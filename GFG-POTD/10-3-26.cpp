a64464a35e372a5be75f3443d971cacec4d1bd43
shubham-mehta-002 <mehta.shubham002@gmail.com>
2026-03-10 11:16:22 +0530

    10-3-26

---
+++ GFG-POTD/10-3-26.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+\class Solution
+{
+public:
+    int countSubarrays(vector<int> &arr)
+    {
+        // next smallest element
+        int n = arr.size();
+
+        stack<int> st;
+        int count = 0;
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            while (!st.empty() && arr[st.top()] >= arr[i])
+            {
+                st.pop();
+            }
+
+            if (st.empty())
+            {
+                count += n - i;
+            }
+            else
+            {
+                count += st.top() - i;
+            }
+
+            st.push(i);
+        }
+
+        return count;
+    }
+};
