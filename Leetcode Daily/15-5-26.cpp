f6c744eea5158e95d7c00c054dbe8933c76c2240
shubham-mehta-002 <mehta.shubham002@gmail.com>
2026-05-15 10:30:00 +0530

    15-5-26

---
+++ Leetcode Daily/15-5-26.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int findMin(vector<int> &nums)
+    {
+        int low = 0;
+        int high = nums.size() - 1;
+
+        int minVal = INT_MAX;
+
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+
+            if (nums[low] <= nums[mid])
+            {
+                minVal = min(minVal, nums[low]);
+                low = mid + 1;
+            }
+            else
+            {
+                minVal = min(minVal, nums[mid]);
+                high = mid - 1;
+            }
+        }
+
+        return minVal;
+    }
+};
