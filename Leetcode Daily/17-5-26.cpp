c570aa5ca13827300cf7281c70cbd0c2dd95825a
shubham-mehta-002 <mehta.shubham002@gmail.com>
2026-05-17 14:30:00 +0530

    17-5-26

---
+++ Leetcode Daily/17-5-26.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    unordered_map<int, bool> mem;
+
+public:
+    bool dfs(int index, vector<int> &arr, vector<int> &vis)
+    {
+        vis[index] = 1;
+
+        if (mem.contains(index))
+        {
+            return mem[index];
+        }
+        if (arr[index] == 0)
+            return mem[index] = true;
+
+        // left
+        if (index - arr[index] >= 0 && !vis[index - arr[index]] && dfs(index - arr[index], arr, vis))
+        {
+            return mem[index] = true;
+        }
+
+        // right
+        if (index + arr[index] < arr.size() && !vis[index + arr[index]] && dfs(index + arr[index], arr, vis))
+        {
+            return mem[index] = true;
+        }
+
+        vis[index] = 0;
+
+        return mem[index] = false;
+    }
+    bool canReach(vector<int> &arr, int start)
+    {
+        int n = arr.size();
+        vector<int> vis(n, 0);
+
+        return dfs(start, arr, vis);
+    }
+};
