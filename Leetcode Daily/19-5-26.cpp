418ed820c28b154b143b1d5d2e83968cb7e5124c
shubham-mehta-002 <mehta.shubham002@gmail.com>
2026-05-19 10:30:00 +0530

    19-5-26

---
+++ Leetcode Daily/19-5-26.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int getCommon(vector<int> &nums1, vector<int> &nums2)
+    {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+
+        int i = 0;
+        int j = 0;
+        while (i < n1 && j < n2)
+        {
+            if (nums1[i] > nums2[j])
+            {
+                j++;
+            }
+            else if (nums1[i] < nums2[j])
+            {
+                i++;
+            }
+            else
+            {
+                return nums1[i];
+            }
+        }
+
+        return -1;
+    }
+};
