b45b8b0359d73712f8bd7ec757bce2677427e3ad
shubham-mehta-002 <mehta.shubham002@gmail.com>
2026-05-22 10:30:00 +0530

    22-5-26

---
+++ Leetcode Daily/22-5-26.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int l = 0;
+        int h = nums.size() - 1;
+
+        while (l <= h)
+        {
+            int mid = (l + h) / 2;
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            else if (nums[l] <= nums[mid])
+            {
+                if (nums[l] <= target && nums[mid] >= target)
+                {
+                    h = mid - 1;
+                }
+                else
+                {
+                    l = mid + 1;
+                }
+            }
+            else
+            {
+                if (nums[mid] <= target && target <= nums[h])
+                {
+                    l = mid + 1;
+                }
+                else
+                {
+                    h = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
