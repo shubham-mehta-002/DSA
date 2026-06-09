0d4211ef1e0cc5cb45f249289eeea76e6b19a2fa
shubham-mehta-002 <mehta.shubham002@gmail.com>
2026-06-09 12:28:29 +0530

    9-6-25

---
+++ Leetcode Daily/9-6-26.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+
+{
+public:
+    long long maxTotalValue(vector<int> &nums, int k)
+    {
+        int maxx = 0;
+        int minn = INT_MAX;
+
+        for (int n : nums)
+        {
+            maxx = max(maxx, n);
+            minn = min(minn, n);
+        }
+
+        return (1LL * k * (maxx - minn));
+    }
+};
