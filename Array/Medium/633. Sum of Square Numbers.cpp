ac38b12f87890df56d5d3c47981acf5478fc6893
shubham-mehta-002 <mehta.shubham002@gmail.com>
2026-08-10 19:14:00 +0530

    10-8-26

---
+++ Array/Medium/633. Sum of Square Numbers.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool judgeSquareSum(int c)
+    {
+        int i = 0;
+        int j = sqrt(c);
+
+        while (i <= j)
+        {
+            long long sum = 1LL * i * i + 1LL * j * j;
+            if (sum == c)
+                return true;
+            if (sum < c)
+                i++;
+            else
+                j--;
+        }
+        return false;
+    }
+};
